44bc4f7a82ec2b934af2f44896ecf1a2b07ef8c4
lipaosbk <felipe_udt@hotmail.com>
2025-06-02 22:09:41 -0300

    primeiro commit personal

---
+++ script.js
@@ -0,0 +1,9 @@
+// script.js
+
+// Seleciona o elemento de contato
+const contatoSection = document.getElementById('contato');
+
+// Adiciona um evento de clique ao elemento de contato
+contatoSection.addEventListener('click', function () {
+    alert('Entre em contato pelo email: cristiano.thomaz@email.com'); // Exibe um alerta com informações de contato
+});
